cf25770106cdee5a4f503b5ba69bf1a419fe77ce
jihwan <jihwan1535@nate.com>
2023-11-13 14:56:12 +0900

    refactor(Order): 메뉴, 수량, 방문일자 정보로 주문을 하도록 수정

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -3,31 +3,47 @@ package christmas.domain;
 import christmas.utility.IllegalArgumentMessage;
 
 public class Order {
-    Menu foodMenu;
-    public Order(String foodMenu) {
-        this.foodMenu = Menu.getMenu(foodMenu);
-        validate();
+    private final Menu menu;
+    private final int quantity;
+    private final int reservationDate;
+    public Order(String menuName, int quantity, int reservationDate) {
+        this.menu = Menu.getMenu(menuName);
+        validate(quantity);
+        this.quantity = quantity;
+        this.reservationDate = reservationDate;
     }
 
-    private void validate() {
-        if(foodMenu.equals(Menu.ETC)) {
+    private void validate(int quantity) {
+        final int minimumQuantity = 1;
+        if(menu.equals(Menu.ETC)) {
+            throw new IllegalArgumentException(IllegalArgumentMessage.INVALID_ORDER);
+        }
+        if(quantity < minimumQuantity) {
             throw new IllegalArgumentException(IllegalArgumentMessage.INVALID_ORDER);
         }
     }
 
-    public Menu getFoodMenu() {
-        return foodMenu;
+    public Menu getMenu() {
+        return menu;
     }
 
     public String getFoodName() {
-        return foodMenu.getName();
+        return menu.getName();
     }
 
     public String getFoodType() {
-        return foodMenu.getType();
+        return menu.getType();
     }
 
     public int getFoodPrice() {
-        return foodMenu.getPrice();
+        return menu.getPrice();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getReservationDate() {
+        return reservationDate;
     }
 }
