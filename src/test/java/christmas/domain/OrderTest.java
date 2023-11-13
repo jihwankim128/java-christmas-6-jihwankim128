fc6be8442a656ef892518eb42ee4b006fa2c7370
jihwan <jihwan1535@nate.com>
2023-11-13 15:15:54 +0900

    refactor(OrderTest): 변경된 Order domain에 대해 메뉴 존재 여부, 메뉴 수량에 대한 재테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.utility.IllegalArgumentMessage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,37 +14,59 @@ class OrderTest {
     Order order;
 
     @ParameterizedTest
-    @CsvSource({"양송이수프,애피타이저,6000", "타파스,애피타이저,5500", "시저샐러드,애피타이저,8000"})
-    void 애피타이저_메뉴_테스트(String menuName, String menuType, int menuPrice) {
-        order = new Order(menuName);
+    @CsvSource({
+            "양송이수프,애피타이저,6000,4,20",
+            "타파스,애피타이저,5500,2,20",
+            "시저샐러드,애피타이저,8000,1,20"
+    })
+    void 애피타이저_메뉴_테스트(String menuName, String menuType, int menuPrice, int quantity, int reservationDate) {
+        order = new Order(menuName, quantity, reservationDate);
         assertEqualResult(menuName, menuType, menuPrice);
     }
 
     @ParameterizedTest
-    @CsvSource({"티본스테이크,메인,55000","바비큐립,메인,54000","해산물파스타,메인,35000","크리스마스파스타,메인,25_000"})
-    void 메인_메뉴_테스트(String menuName, String menuType, int menuPrice) {
-        order = new Order(menuName);
+    @CsvSource({
+            "티본스테이크,메인,55000,4,20",
+            "바비큐립,메인,54000,2,20",
+            "해산물파스타,메인,35000,1,20",
+            "크리스마스파스타,메인,25_000,5,15"
+    })
+    void 메인_메뉴_테스트(String menuName, String menuType, int menuPrice, int quantity, int reservationDate) {
+        order = new Order(menuName, quantity, reservationDate);
         assertEqualResult(menuName, menuType, menuPrice);
     }
 
     @ParameterizedTest
-    @CsvSource({"초코케이크,디저트,15000", "아이스크림,디저트,5000"})
-    void 디저트_메뉴_테스트(String menuName, String menuType, int menuPrice) {
-        order = new Order(menuName);
+    @CsvSource({"초코케이크,디저트,15000,4,20", "아이스크림,디저트,5000,2,20"})
+    void 디저트_메뉴_테스트(String menuName, String menuType, int menuPrice, int quantity, int reservationDate) {
+        order = new Order(menuName, quantity, reservationDate);
         assertEqualResult(menuName, menuType, menuPrice);
     }
 
     @ParameterizedTest
-    @CsvSource({"제로콜라,음료,3000", "레드와인,음료,60000", "샴페인,음료,25000"})
-    void 음료_메뉴_테스트(String menuName, String menuType, int menuPrice) {
-        order = new Order(menuName);
+    @CsvSource({
+            "제로콜라,음료,3000,10,10,4,20",
+            "레드와인,음료,60000,5,5,2,20",
+            "샴페인,음료,25000,4,4,1,20"})
+    void 음료_메뉴_테스트(String menuName, String menuType, int menuPrice, int quantity, int reservationDate) {
+        order = new Order(menuName, quantity, reservationDate);
         assertEqualResult(menuName, menuType, menuPrice);
     }
 
     @ParameterizedTest
-    @CsvSource({"포비,메인,100", "오리,디저트,0", "x,x,0"})
-    void 없는_메뉴_테스트(String menuName, String menuType, int menuPrice) {
-        assertThatThrownBy(()-> new Order(menuName))
+    @CsvSource({"포비,메인,100,4,20", "오리,디저트,0,2,20", "x,x,0,1,20"})
+    void 없는_메뉴_테스트(String menuName, String menuType, int menuPrice, int quantity, int reservationDate) {
+        assertThatThrownBy(()-> new Order(menuName, quantity, reservationDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentMessage.INVALID_ORDER);
+    }
+
+    @Test
+    void 최소_수량_테스트() {
+        final String menuName = "제로콜라";
+        final int quantity = 0;
+        final int reservationDate = 10;
+        assertThatThrownBy(()-> new Order(menuName, quantity, reservationDate))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(IllegalArgumentMessage.INVALID_ORDER);
     }
