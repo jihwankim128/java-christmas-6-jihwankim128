4a38df72547a290ef3bbfe978ccaf7c71459d371
jihwan <jihwan1535@nate.com>
2023-11-14 21:03:51 +0900

    feat(WeekDayBenefit): 평일 이벤트 혜택을 처리하는 domain 추가

---
+++ src/main/java/christmas/domain/orderdetails/benefit/WeekDayBenefit.java
@@ -0,0 +1,37 @@
+package christmas.domain.orderdetails.benefit;
+
+import christmas.domain.Event;
+import christmas.domain.Order;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class WeekDayBenefit {
+    public static final int DISCOUNT_PRICE = 2023;
+    private int weekDayBenefit;
+
+    public WeekDayBenefit() {
+        this.weekDayBenefit = 0;
+    }
+
+    public int getWeekDayBenefit() {
+        return weekDayBenefit;
+    }
+
+    public void applyWeekDayBenefit(List<Order> orders, Event event) {
+        if (event.isWeekDayEvent()) {
+            this.weekDayBenefit =
+                    (int) orders.stream()
+                            .map(order -> order.getMenuType().equals("디저트"))
+                            .count() * DISCOUNT_PRICE;
+        }
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+        return "평일 할인: "
+                + formatter.format(-weekDayBenefit)
+                + "원\n";
+    }
+}
