f1478cf210ec5245e960e0fe9a11ac8b851442cf
jihwan <jihwan1535@nate.com>
2023-11-14 16:11:38 +0900

    test(TotalPrice): 총주문 금액이 올바르게 계산되었는지 확인하고 toString() 출력값을 확인하는 테스트

---
+++ src/test/java/christmas/domain/orderdetails/TotalPriceTest.java
@@ -0,0 +1,33 @@
+package christmas.domain.orderdetails;
+
+import static org.assertj.core.api.Assertions.*;
+
+import christmas.domain.Order;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("총주문 금액 테스트")
+class TotalPriceTest {
+    @Test
+    void 총_주문_금액() {
+        TotalPrice totalPrice = new TotalPrice(Arrays.asList(
+                new Order("티본스테이크", 1),
+                new Order("바비큐립", 1),
+                new Order("초코케이크", 2),
+                new Order("제로콜라", 1)
+        ));
+        assertThat(totalPrice.getTotalPrice()).isEqualTo(142000);
+    }
+
+    @Test
+    void 총_주문_금액_출력() {
+        TotalPrice totalPrice = new TotalPrice(Arrays.asList(
+                new Order("티본스테이크", 1),
+                new Order("바비큐립", 1),
+                new Order("초코케이크", 2),
+                new Order("제로콜라", 1)
+        ));
+        assertThat(totalPrice.toString()).isEqualTo("142,000원");
+    }
+}
