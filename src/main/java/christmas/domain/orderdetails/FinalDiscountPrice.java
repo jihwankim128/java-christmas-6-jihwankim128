3c5148571f31f3aa92021355b114f02623e31665
jihwan <jihwan1535@nate.com>
2023-11-14 22:23:14 +0900

    feat(FinalDiscountPrice): 모든 혜택을 적용해서 최종적으로 할인받게 될 혜택금액을 처리하는 domain 추가

---
+++ src/main/java/christmas/domain/orderdetails/FinalDiscountPrice.java
@@ -0,0 +1,31 @@
+package christmas.domain.orderdetails;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class FinalDiscountPrice {
+    private int discountPrice;
+
+    public FinalDiscountPrice(Benefit benefit) {
+        this.discountPrice = 0;
+        calculateAppliedBenefits(benefit);
+    }
+
+    private void calculateAppliedBenefits(Benefit benefit) {
+        this.discountPrice += benefit.getGiftBenefit();
+        this.discountPrice += benefit.getWeekDayBenefit();
+        this.discountPrice += benefit.getWeekEndBenefit();
+        this.discountPrice += benefit.getSpecialBenefit();
+        this.discountPrice += benefit.getGiftBenefit();
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+        return formatter.format(-discountPrice) + "원\n";
+    }
+}
