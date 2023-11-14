2860094551a13441bd20d53dfe51507671485310
jihwan <jihwan1535@nate.com>
2023-11-14 21:20:44 +0900

    feat(SpecialBenefit): 특별 이벤트 혜택을 처리하는 domain 추가

---
+++ src/main/java/christmas/domain/orderdetails/benefit/SpecialBenefit.java
@@ -0,0 +1,33 @@
+package christmas.domain.orderdetails.benefit;
+
+import christmas.domain.Event;
+import christmas.domain.Menu;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class SpecialBenefit {
+    public static final int SPECIAL_BENEFIT_PRICE = 1000;
+    private int specialBenefit;
+
+    public SpecialBenefit() {
+        this.specialBenefit = 0;
+    }
+
+    public void applySpecialBenefit(Event event) {
+        if (event.isSpecialEvent()) {
+            this.specialBenefit = SPECIAL_BENEFIT_PRICE;
+        }
+    }
+
+    public int getSpecialBenefit() {
+        return specialBenefit;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+        return "평일 할인: "
+                + formatter.format(-specialBenefit)
+                + "원\n";
+    }
+}
