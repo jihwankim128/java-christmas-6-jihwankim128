03713cfcd469c90567947e072755d63de1b69c95
jihwan <jihwan1535@nate.com>
2023-11-15 22:07:25 +0900

    fix: 컨트롤러를 static으로 사용하여 이전 주문정보를 controller에 존재하는 버그 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -7,22 +7,29 @@ import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Application {
-    private static final InputView inputView = new InputView();
-    private static final OutputView outputView = new OutputView();
-    private static final EventController eventController = new EventController(inputView);
-    private static final OrderController orderController = new OrderController(inputView);
-
     public static void main(String[] args) {
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+        final EventController eventController = new EventController(inputView);
+        final OrderController orderController = new OrderController(inputView);
+        reservationRegister(inputView, outputView, eventController, orderController);
+
+        final OrderDetailsController orderDetailsController
+                = new OrderDetailsController(outputView, eventController, orderController);
+        orderDetailsController.orderDetailsRegister();
+        orderDetailsController.orderDetailsPrint();
+    }
+
+    private static void reservationRegister(
+            InputView inputView,
+            OutputView outputView,
+            EventController eventController,
+            OrderController orderController
+    ) {
         outputView.eventGreetingPrint();
         eventController.eventRegister();
 
         orderController.ordersRegister();
         outputView.eventNoticePrint(eventController.getReservationDate());
-
-        OrderDetailsController orderDetailsController
-                = new OrderDetailsController(outputView, eventController, orderController);
-
-        orderDetailsController.orderDetailsRegister();
-        orderDetailsController.orderDetailsPrint();
     }
 }
