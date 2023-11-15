67086d307f2e74ce009da0b2f7fe09c3a5548802
jihwankim128 <jihwan1535@nate.com>
2023-11-15 09:36:27 +0900

    refactor(Oreder): 객체화 하여 Controller에서 기능을 사용하여 관리할 수 있도록 수정

--- src/main/java/christmas/domain/OrderDetails.java
+++ src/main/java/christmas/domain/OrderDetails.java
@@ -3,41 +3,44 @@ package christmas.domain;
 import christmas.domain.orderdetails.Benefit;
 import christmas.domain.orderdetails.EventBadge;
 import christmas.domain.orderdetails.TotalDiscountAmount;
-import christmas.domain.orderdetails.FinalPrice;
+import christmas.domain.orderdetails.TotalPriceAfterBenefit;
 import christmas.domain.orderdetails.GiftMenu;
 import christmas.domain.orderdetails.Orders;
-import christmas.domain.orderdetails.TotalPrice;
+import christmas.domain.orderdetails.TotalPriceBeforeBenefit;
 import java.util.List;
 
 public class OrderDetails {
-    private Orders orders;
-    private TotalPrice totalPrice;
-    private GiftMenu giftMenu;
-    private Benefit benefit;
-    private TotalDiscountAmount totalDiscountAmount;
-    private FinalPrice finalPrice;
-    private EventBadge badge;
+    private final Orders orders;
+    private final TotalPriceBeforeBenefit totalPriceBeforeBenefit;
+    private final TotalPriceAfterBenefit totalPriceAfterBenefit;
+    private final GiftMenu giftMenu = new GiftMenu();
+    private final Benefit benefit = new Benefit();
+    private final TotalDiscountAmount totalDiscountAmount = new TotalDiscountAmount();
 
-    OrderDetails(List<Order> orders, Event event) {
-        initOrderDetails(orders, event);
+    OrderDetails(List<Order> orders) {
+        this.orders = new Orders(orders);
+        this.totalPriceBeforeBenefit = new TotalPriceBeforeBenefit(orders);
+        this.totalPriceAfterBenefit = new TotalPriceAfterBenefit(totalPriceBeforeBenefit.getTotalPrice());
     }
 
-    private void initOrderDetails(List<Order> orders, Event event) {
-        this.orders = new Orders(orders);
-        this.totalPrice = new TotalPrice(orders);
-        this.giftMenu = new GiftMenu(totalPrice, event);
-        this.benefit = new Benefit(orders, totalPrice, event);
-        this.totalDiscountAmount = new TotalDiscountAmount(benefit);
-        this.finalPrice = new FinalPrice(totalPrice, totalDiscountAmount, event);
-        this.badge = EventBadge.getBadge(totalDiscountAmount.getDiscountPrice());
+    public void provideGift(Event event) {
+        if(event.isGiftEvent(totalPriceBeforeBenefit.getTotalPrice())) {
+            this.giftMenu.provide();
+        }
+    }
+
+    public void applyBenefit(Event event) {
+        this.benefit.applyBenefit(orders.getOrders(), totalPriceBeforeBenefit.getTotalPrice(), event);
+        this.totalDiscountAmount.setDiscountAmount(benefit.totalDiscountAmount());
+        this.totalPriceAfterBenefit.applyDiscountAmount(totalDiscountAmount.getDiscountAmount(), giftMenu.price());
     }
 
     public Orders getOrders() {
         return orders;
     }
 
-    public TotalPrice getTotalPrice() {
-        return totalPrice;
+    public TotalPriceBeforeBenefit getTotalPrice() {
+        return totalPriceBeforeBenefit;
     }
 
     public GiftMenu getGiftMenu() {
@@ -52,11 +55,11 @@ public class OrderDetails {
         return totalDiscountAmount;
     }
 
-    public FinalPrice getFinalPrice() {
-        return finalPrice;
+    public TotalPriceAfterBenefit getFinalPrice() {
+        return totalPriceAfterBenefit;
     }
 
     public EventBadge getBadge() {
-        return badge;
+        return EventBadge.getBadge(totalDiscountAmount.getDiscountAmount());
     }
 }
