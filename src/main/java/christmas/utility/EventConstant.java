4ec6481a3543ab5f4484356a8015ce9f6285b29e
jihwan <jihwan1535@nate.com>
2023-11-15 17:36:41 +0900

    feat: domain별 특징을 가지고 있는 상수들을 정리하여 final 클래스를 추가

---
+++ src/main/java/christmas/utility/EventConstant.java
@@ -0,0 +1,18 @@
+package christmas.utility;
+
+public final class EventConstant {
+    public static final int GIFT_EVENT_MINIMUM_PAYMENT = 120000;
+    public static final int DISCOUNT_EVENT_MINIMUM_TOTAL_PRICE = 10000;
+
+    public static final int D_DAY_BASE_BENEFIT = 1000;
+    public static final int INCREASE_BASE_PER_DAY = 100;
+
+    public static final int EVENT_START_DATE = 1;
+    public static final int EVENT_END_DATE = 31;
+    public static final int CHRISTMAS_DATE = 25;
+
+    public static final int DAYS_IN_WEEK = 7;
+    public static final int FRIDAY = 1;
+    public static final int SATURDAY = 2;
+    public static final int SUNDAY = 3;
+}
---
+++ src/main/java/christmas/utility/OrderConstant.java
@@ -0,0 +1,14 @@
+package christmas.utility;
+
+public class OrderConstant {
+    public static final int MINIMUM_QUANTITY = 1;
+    public static final int MAX_ORDER_QUANTITY = 20;
+    public static final int DISCOUNT_PRICE = 2023;
+    public static final int SPECIAL_BENEFIT_PRICE = 1000;
+
+    public static final String APPETIZER = "애피타이저";
+    public static final String MAIN = "메인";
+    public static final String DESSERT = "디저트";
+    public static final String DRINK = "음료";
+    public static final String NONE = "없음\n";
+}
